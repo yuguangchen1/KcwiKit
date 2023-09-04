adfb979c09c89fae2c7e6eebcc2e2b0cdc3d5c05
Nikolaus Prusinski <nik@astro.caltech.edu>
2023-09-04 15:13:28 -0700

    updating multiple_grangles part 2

--- py/kcwi.py
+++ py/kcwi.py
@@ -915,6 +915,10 @@ def kcwi_stack(fnlist,shiftlist='',preshiftfn='',fluxfn='',pixscale_x=0.,pixscal
                overwrite=False,keep_trim=True,keep_mont=True,method='drizzle',use_astrom=False,
                use_regmask=True, low_mem=False, montagepy=False, crr=False, crr_save_files=False,
                crrthresh=100, medcube=False, nsigma_clip=1.5, npix_trim = 3, multiple_grangles=False, mg_wavgood = False):
+<<<<<<< HEAD
+=======
+
+>>>>>>> aca8383 (updating multiple_grangles part 2)
     """
     Stacking the individual data cubes.
 
