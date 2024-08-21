f84f31c92f8693135da4f2eb8429e5acb6460042
Yuguang Chen <yuguang.chen.1@gmail.com>
2024-08-21 12:56:07 -0700

    consolidated kcwi_makemask_medfilter for icube and icubed files

--- pyDRP/kcwi_makemask_medfilter.py
+++ pyDRP/kcwi_makemask_medfilter.py
@@ -1,34 +1,68 @@
 #!/usr/bin/env python
 
 """
-Converting region files to sky masks. Same as kcwi_makemask.pro.
+Converting region files to sky masks specifically for median filtering. Similar to kcwi_makemask.pro.
 """
 
 import glob, os, sys
-import pdb
+import argparse
 
 import pathlib
 fileloc = str(pathlib.Path(__file__).parent.resolve())
 
-narg=len(sys.argv)
-if narg==1:
-    dir='./'
-if narg==2:
-    dir=sys.argv[1]
-if narg>2:
-    print('Incorrect number of arguments.')
-    exit()
+def parser_init():
+    """Create command-line argument parser for this script."""
+    parser = argparse.ArgumentParser(description="Convert region masks to binary masks.")
+    parser.add_argument(
+        'dir',
+        type=str,
+        help='Directory name in which region files are located.',
+        default='./',
+        nargs='?'
+        )
+    parser.add_argument(
+        '-d', '--cubed',
+        help='Act on DAR corrected cubed files.',
+        action='store_true'
+        )
+    return parser
 
-for regfn in glob.glob(dir+"/*_icube.thum.reg"):
-    #pdb.set_trace()
+def makemask(dir, cubed=False):
+    """Converting region files to sky masks specifically for median filtering.
 
-    imgfn=regfn.replace('.reg','.fits')
-    os.system(fileloc+"/kcwi_masksky_ds9_thum.py "+imgfn+" "+regfn)
+    Args:
+        dir (str): directory of which the region files are stored.
 
-for regfn in glob.glob(dir+"/*_icube_2d.reg"):
-    #pdb.set_trace()
+    Returns:
+        None. Region files are converted to binary FITS files. 
 
-    imgfn=regfn.replace('.reg','.fits')
-    os.system(fileloc+"/kcwi_masksky_ds9_2d.py "+imgfn+" "+regfn)
+    """
 
-print('Sky Masks Complete')
+    if not cubed:
+        search_for = dir + "/*_icube.thum.reg"
+    else:
+        search_for = dir + '/*_icubed.thum.reg'
+
+    for regfn in glob.glob(search_for):
+        imgfn=regfn.replace('.reg','.fits')
+        os.system(fileloc+"/kcwi_masksky_ds9_thum.py "+imgfn+" "+regfn)
+
+    if not cubed:
+        search_for = dir + "/*_icube_2d.reg"
+    else:
+        search_for = dir + "/*_icubed_2d.reg"
+
+    for regfn in glob.glob(search_for):
+        imgfn=regfn.replace('.reg','.fits')
+        os.system(fileloc+"/kcwi_masksky_ds9_2d.py "+imgfn+" "+regfn)
+
+    print('Sky Masks Complete')
+
+def main():
+    arg_parser = parser_init()
+    args = arg_parser.parse_args()
+    makemask(**vars(args))
+
+#Call if run from command-line
+if __name__ == "__main__":
+    main()
